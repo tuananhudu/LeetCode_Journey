3d1aa1fa4be5f138e5b7e88beb5c316f2f22be48
tuananhudu <buiduytuananhudu@gmail.com>
2025-05-13 19:17:48 +0700

    add Class Ex6

--- Generation_Algorithms/Ex6.py
+++ Generation_Algorithms/Ex6.py
@@ -27,44 +27,89 @@
 
 # Bài Làm 
 # ----------------Cách1------------------------- #
-def ktao(n , a):
-    for i in range(1, n+1):
-        a[i] = i 
-
-def sinh():
-    global ok , a , n 
-    i = n - 1 
-    while(i > 0 and a[i] > a[i+1]):
-        i -=1 
-    if i == 0 : 
-        ok = False 
-    else : 
-        j = n 
-        while(i < j):
-            if (a[i] < a[j]):
-                a[i] , a[j] = a[j] , a[i]
-                a[i+1:] = a[i+1:][::-1]
-                break
-            else : 
-                j -= 1 
-
+class SinhHoanVi():
+    def __init__(self , n):
+        self.n = n 
+        self.ok = True 
+        self.a = [0] * (n+1)
+    def ktao(self):
+        for i in range(1 , self.n+1):
+            self.a[i] = i 
+        return self 
+    def sinh(self):
+        i = self.n - 1 
+        while(i > 0 and self.a[i] >self.a[i+1]):
+            i -= 1 
+        if i == 0 : 
+            self.ok = False 
+        else : 
+            j = self.n
+            while(i < j):
+                if (self.a[i] < self.a[j]):
+                    self.a[i] , self.a[j] = self.a[j] , self.a[i]
+                    self.a[i+1:] = self.a[i+1:][::-1]
+                    break 
+                else : 
+                    j -= 1
+    def sinh_all(self):
+        self.ktao()
+        res = ''
+        results = []
+        while(self.ok):
+            for i in range(1 , self.n+1):
+                res += str(self.a[i])
+            results.append(res)
+            res = ''
+            self.sinh()
+        for i in results[::-1]:
+            print(int(i) , end = ' ')
 if __name__ == '__main__':
     t = int(input())
     for _ in range(t):
         n = int(input())
-        ok = 1
-        a = [0] * (n+1)
-        ktao(n , a)
-        res = ''
-        result = []
-        while(ok):
-            for i in range(1 , n+1):
-                # res += (a[i] - '0')
-                res += str(a[i])
-            result.append(res)
-            res = ''
-            sinh()
-        for i in result[::-1] :
-            print(int(i) , end =' ') 
+        a = SinhHoanVi(n)
+        a.sinh_all()
         print()
+
+# ----------------Cách2------------------------- #
+# def ktao(n , a):
+#     for i in range(1, n+1):
+#         a[i] = i 
+
+# def sinh():
+#     global ok , a , n 
+#     i = n - 1 
+#     while(i > 0 and a[i] > a[i+1]):
+#         i -=1 
+#     if i == 0 : 
+#         ok = False 
+#     else : 
+#         j = n 
+#         while(i < j):
+#             if (a[i] < a[j]):
+#                 a[i] , a[j] = a[j] , a[i]
+#                 a[i+1:] = a[i+1:][::-1]
+#                 break
+#             else : 
+#                 j -= 1 
+
+# if __name__ == '__main__':
+#     t = int(input())
+#     for _ in range(t):
+#         n = int(input())
+#         ok = 1
+#         a = [0] * (n+1)
+#         ktao(n , a)
+#         res = ''
+#         result = []
+#         while(ok):
+#             for i in range(1 , n+1):
+#                 # res += (a[i] - '0')
+#                 res += str(a[i])
+#             result.append(res)
+#             res = ''
+#             sinh()
+#         for i in result[::-1] :
+#             print(int(i) , end =' ') 
+#         print()
 
